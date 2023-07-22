4c23c8f4800aedf2876e3a6ba54bdca00053ab21
Row-Nish <sronish21@tbc.edu.np>
2023-07-22 20:25:52 +0545

    my commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css'
+import tempImage from './assets/temp.png';
 
 function App() {
+
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState("")
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=5eebde1fe0fbe20de343a4a7a845cb37`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
+  // Farenheit to Celcius 
+  const toCelsius = (tempFarenheit) => {
+    return ((tempFarenheit - 32) * 5 / 9).toFixed(2);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='app' >
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyDown={searchLocation}
+          placeholder="Enter Location"
+          type="text" />
+      </div>
+      <div className="container">
+        <div className="location">
+          <span>{data.name}</span>
+        </div>
+        <div className="sub-container">
+          <div className="left-container">
+
+            <div className="img">
+              <img src={tempImage} alt="thermometer " />
+            </div>
+            <div className="temp">
+              {data.main ? <p>{toCelsius(data.main.temp)}°C</p> : null}
+
+              <div className="min-temp">
+                {data.main ? <p>Min: {toCelsius(data.main.temp_min)}°C</p> : null}
+              </div>
+              <div className="max-temp">
+                {data.main ? <p>Max: {toCelsius(data.main.temp_max)}°C</p> : null}
+              </div>
+            </div>
+          </div>
+          <div className="right-container">
+
+            <div className="Pressure">
+              {data.main ? <p>{data.main.pressure}</p> : null}
+              <p>Pressure</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p>{data.main.humidity}%</p> : null}
+              <p>Humidity </p>
+            </div>
+          </div>
+        </div>
+        <div className="description">
+          {data.weather ? <h1>{data.weather[0].main}</h1> : null}
+        </div>
+
+
+
+      </div>
+    </div >
+
   );
 }
+
+
 
 export default App;
